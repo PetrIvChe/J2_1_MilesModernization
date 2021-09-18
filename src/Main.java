0d2534a84d33b26282bfe56caf64dcf6f8a7bfc4
PetrIvChe <cherniavskiipetr@gmail.com>
2021-09-18 20:09:54 +0300

    Create class main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketCost = 10321;
+        int rublesPerMyle = 20;
+        int bonusRubles = ticketCost / rublesPerMyle;
+        System.out.println("Вам начислено " + bonusRubles + " бонусных рублей.");
+    }
+}
+
